3c54d786bcb5d0f41468242275adc1b016fa2629
minamarin <120408542+minamarin@users.noreply.github.com>
2024-05-08 11:13:10 -0400

    message working

--- react-chatroom/src/App.jsx
+++ react-chatroom/src/App.jsx
@@ -9,22 +9,19 @@ function App() {
     // GET request using fetch inside useEffect React hook
     fetch('https://ohwuvawrw4.execute-api.us-west-2.amazonaws.com/messages')
       .then((response) => response.json())
-      .then((data) => setMessageArray(data))
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+      .then((data) => setMessageArray(data));
+    // empty dependency array means this effect will only run once
   }, []);
-
-  messageArray.map((element, index) => {
-    return <h1 key={index}>{element.message}</h1>
-  })
 
   return (
     <>
       <h1> hello world </h1>
-      <Message messageArray={messageArray}></Message>
+      {messageArray.map((element, index) => (
+        <p key={index}>{element.message}</p>
+      ))}
+      {/* <Message messageArray={messageArray}></Message> */}
     </>
   );
 }
-
-
 
 export default App;
--- react-chatroom/src/message.jsx
+++ react-chatroom/src/message.jsx
@@ -19,9 +19,12 @@ const Message = (props) => {
   // }, []);
 
   for (let i = 0; i < props.messageArray.length - 1; i++) {}
-
-  //   fetchData();
-  return <>props.messageArray</>;
+  //   return <>props.messageArray</>;
+  return (
+    <div className='message-container'>
+      <Containers box={props.messageArray} fetchData={fetchData} />
+    </div>
+  );
 };
 
 function fetchData() {
@@ -30,21 +33,99 @@ function fetchData() {
     .then((data) => displayMessages(data));
 }
 
-function displayMessages(data) {
-  sessionStorage.setItem('lastMsgTs', data[0].created_at);
+// function createChatArea() {
+//   const body = document.querySelector('body');
 
-  let batchLength = 500;
-  if (data.length < batchLength) batchLength = data.length;
+//   const chatArea = document.createElement('div');
+//   body.appendChild(chatArea);
+//   chatArea.setAttribute('id', 'chatArea');
+//   chatArea.style.border = '1px solid black';
+//   chatArea.style.height = '600px';
+//   chatArea.style.width = '1300px';
+//   chatArea.style.margin = 'auto';
+//   chatArea.style.backgroundColor = '#F4EDEA';
+//   chatArea.style.overflowY = 'scroll';
 
-  for (let i = batchLength - 1; i >= 0; i--) {
-    let messageDiv;
-    if (data[i].created_by === sessionStorage.user)
-      messageDiv = createMessageDiv('user');
-    else messageDiv = createMessageDiv();
+//   const userInputWrapper = document.createElement('div');
+//   body.appendChild(userInputWrapper);
+//   userInputWrapper.style.width = '1300px';
+//   userInputWrapper.style.margin = 'auto';
 
-    messageDiv.querySelector('#messageName').innerHTML = data[i].created_by;
-    messageDiv.querySelector('#messageText').innerHTML = data[i].message;
-  }
-}
+//   // every time a user enter text into textInputField, the text is saved in the textInputField.value property
+//   const textInputField = document.createElement('input');
+//   userInputWrapper.appendChild(textInputField);
+//   textInputField.setAttribute('id', 'textField');
+//   textInputField.setAttribute('type', 'text');
+//   textInputField.setAttribute('placeholder', 'Type a message');
+//   textInputField.style.width = '1200px';
+
+//   const button = document.createElement('input');
+//   userInputWrapper.appendChild(button);
+//   button.setAttribute('id', 'button');
+//   button.setAttribute('type', 'submit');
+//   button.style.width = '100px';
+// }
+
+// function createMessageDiv(arg) {
+//   const message = document.createElement('div');
+//   document.querySelector('#chatArea').appendChild(message);
+//   message.style.border = '1px solid grey';
+//   message.style.width = '600px';
+//   message.style.padding = '5px';
+//   message.scrollIntoView(false);
+
+//   if (arguments.length === 0) {
+//     message.style.backgroundColor = '#F4D1AE';
+//     message.style.margin = '10px auto 10px 50px';
+//   } else {
+//     message.style.backgroundColor = '#F7EF99';
+//     message.style.margin = '10px 50px 10px auto';
+//   }
+
+//   const name = document.createElement('p');
+//   message.appendChild(name);
+//   name.setAttribute('id', 'messageName');
+//   name.style.fontWeight = 'bold';
+//   name.style.margin = '0px';
+
+//   const text = document.createElement('p');
+//   message.appendChild(text);
+//   text.setAttribute('id', 'messageText');
+//   text.style.margin = '0px';
+
+//   return message;
+// }
+
+// function displayMessages(data) {
+//   sessionStorage.setItem('lastMsgTs', data[0].created_at);
+
+//   let batchLength = 500;
+//   if (data.length < batchLength) batchLength = data.length;
+
+//   for (let i = batchLength - 1; i >= 0; i--) {
+//     let messageDiv;
+//     if (data[i].created_by === sessionStorage.user)
+//       messageDiv = createMessageDiv('user');
+//     else messageDiv = createMessageDiv();
+
+//     messageDiv.querySelector('#messageName').innerHTML = data[i].created_by;
+//     messageDiv.querySelector('#messageText').innerHTML = data[i].message;
+//   }
+// }
+
+// function postMessage() {
+//   const textField = document.querySelector('#textField');
+//   fetch('https://ohwuvawrw4.execute-api.us-west-2.amazonaws.com/messages', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       message: textField.value,
+//       created_by: sessionStorage.user,
+//     }),
+//   });
+
+//   const messageDiv = createMessageDiv('user');
+//   messageDiv.querySelector('#messageName').innerHTML = sessionStorage.user;
+//   messageDiv.querySelector('#messageText').innerHTML = textField.value;
+// }
 
 export default Message;
