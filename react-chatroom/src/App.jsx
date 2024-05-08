433a22b4b79e2ae2a5a18565bffc0ee8d43362dc
minamarin <120408542+minamarin@users.noreply.github.com>
2024-05-08 17:45:48 -0400

    added a box for the chat

--- react-chatroom/src/App.jsx
+++ react-chatroom/src/App.jsx
@@ -6,21 +6,24 @@ function App() {
   const [messageArray, setMessageArray] = useState([]);
 
   useEffect(() => {
-    // GET request using fetch inside useEffect React hook
     fetch('https://ohwuvawrw4.execute-api.us-west-2.amazonaws.com/messages')
       .then((response) => response.json())
-      .then((data) => setMessageArray(data));
-    // empty dependency array means this effect will only run once
+      .then((data) =>
+        setMessageArray(
+          data.map((element, index) => {
+            return <Message chat={element.message} key={index} />;
+          }).reverse()
+        )
+      );
   }, []);
 
   return (
-    <>
-      <h1> hello world </h1>
-      {messageArray.map((element, index) => (
-        <p key={index}>{element.message}</p>
-      ))}
-      {/* <Message messageArray={messageArray}></Message> */}
-    </>
+    <div className="chat-container">
+      <h1> PTRI CHATS </h1>
+      <div className="message-list">
+        {messageArray}
+      </div>
+    </div>
   );
 }
 
